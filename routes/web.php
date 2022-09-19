2679e9208baac706e21519b20307e7b8fda51818
urdream30 <urdream30@naver.com>
2022-09-19 22:34:38 +0900

    test2

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ Route::get('/', function () {
     // });
 
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
     
