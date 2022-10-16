0421ab2be06e020e7c39fbe7c006b7dc1489ea6b
urdream30 <urdream30@naver.com>
2022-10-16 21:41:30 +0900

    Lesson34

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -19,7 +19,9 @@
                 <button 
                     @click="show = ! show" 
                     class="py-2 pl-3 pr-9 text-sm font-semibold w-32 text-left flex lg:inline-flex">
-                    Categories
+                    {{-- Categories --}}
+
+                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                     height="22" viewBox="0 0 22 22">
@@ -33,9 +35,14 @@
                 </button>
 
                 <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display:none">
+                    <a href="/"
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white"> 
+                        All</a>
+                        
                     @foreach ($categories as $category)
                         <a href="/categories/{{ $category->slug }}"
-                            class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white"> 
+                            class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white
+                            {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}"> 
                             {{ ucwords($category->name) }}</a>
                     @endforeach
                 </div>
